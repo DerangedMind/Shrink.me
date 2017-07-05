48c989e7a12588f40c0fade581bf7081163dc451
Riki <rick.libby.91@gmail.com>
2017-07-05 18:09:05 +0000

    login and logout cookies, no pass

--- express_server.js
+++ express_server.js
@@ -6,40 +6,56 @@ const PORT = process.env.PORT || 8080 // default port 8080
 
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
 
+// GET ----------------------------------
+
 app.get("/", (req, res) => {
   res.render("pages/urls_index", {
-      links: urlDatabase
+      links: urlDatabase,
+      username: req.cookies.username
     })
 })
 
 app.get("/urls/", (req, res) => {
   res.render("pages/urls_index", {
-    links: urlDatabase
+    links: urlDatabase,
+    username: req.cookies.username
   })
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render("pages/urls_new")
+  res.render("pages/urls_new", {
+    username: req.cookies.username
+  })
 })
 
 app.get("/urls/:id", (req, res) => {
   let urlID = { 
     shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies.username
   }
   res.render("pages/urls_show", urlID)
 })
 
-
 app.get("/u/:shortURL", (req, res) => {
-  res.redirect(`${urlDatabase[req.params.shortURL]}`)
+  res.redirect(`${urlDatabase[req.params.shortURL]}`, {
+    username: req.cookies.username
+  })
 })
+
+app.get("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.redirect(`/urls`)
+})
+
+// POST ----------------------------------
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
@@ -60,6 +76,12 @@ app.post("/urls/:id", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id]
+  res.redirect(`/urls`)
+})
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body['username'])
+  console.log(res.cookies, req.body['username']);
   res.redirect(`/urls`)
 })
 
